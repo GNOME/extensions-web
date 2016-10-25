c43f339b92f088f44fae3fb7db5389a90b538a4f
Yuri Konotopov <ykonotopov@gnome.org>
2016-10-26 00:31:17 +0300

    django: upgrade to latest 1.8 LTS. Bump django modules to latest versions.
    Reworked extensions.views.create_version to use transaction.atomic

--- sweettooth/extensions/views.py
+++ sweettooth/extensions/views.py
@@ -13,6 +13,7 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.template.loader import render_to_string
 from django.views.decorators.http import require_POST
 
+from sweettooth.exceptions import DatabaseErrorWithMessages
 from sweettooth.extensions import models, search
 from sweettooth.extensions.forms import UploadForm
 
@@ -366,48 +367,44 @@ def ajax_set_status_view(request, newstatus):
     return dict(svm=json.dumps(extension.visible_shell_version_map),
                 mvs=render_to_string('extensions/multiversion_status.html', context))
 
+
 def create_version(request, file_source):
-    transaction.set_autocommit(False)
     try:
-        try:
-            metadata = models.parse_zipfile_metadata(file_source)
-            uuid = metadata['uuid']
-        except (models.InvalidExtensionData, KeyError), e:
-            messages.error(request, "Invalid extension data: %s" % (e.message,))
-            transaction.rollback()
-            return None, []
+        with transaction.atomic():
+            try:
+                metadata = models.parse_zipfile_metadata(file_source)
+                uuid = metadata['uuid']
+            except (models.InvalidExtensionData, KeyError), e:
+                messages.error(request, "Invalid extension data: %s" % (e.message,))
+                raise DatabaseErrorWithMessages
 
-        try:
-            extension = models.Extension.objects.get(uuid=uuid)
-        except models.Extension.DoesNotExist:
-            extension = models.Extension(creator=request.user)
-        else:
-            if request.user != extension.creator and not request.user.is_superuser:
-                messages.error(request, "An extension with that UUID has already been added.")
-                transaction.rollback()
-                return None, []
+            try:
+                extension = models.Extension.objects.get(uuid=uuid)
+            except models.Extension.DoesNotExist:
+                extension = models.Extension(creator=request.user)
+            else:
+                if request.user != extension.creator and not request.user.is_superuser:
+                    messages.error(request, "An extension with that UUID has already been added.")
+                    raise DatabaseErrorWithMessages
 
-        extension.parse_metadata_json(metadata)
-        extension.save()
+            extension.parse_metadata_json(metadata)
+            extension.save()
 
-        try:
-            extension.full_clean()
-        except ValidationError, e:
-            transaction.rollback()
-            return None, e.messages
+            try:
+                extension.full_clean()
+            except ValidationError, e:
+                raise DatabaseErrorWithMessages(e.messages)
 
-        version = models.ExtensionVersion.objects.create(extension=extension,
-                                                         source=file_source,
-                                                         status=models.STATUS_UNREVIEWED)
-        version.parse_metadata_json(metadata)
-        version.replace_metadata_json()
-        version.save()
+            version = models.ExtensionVersion.objects.create(extension=extension,
+                                                             source=file_source,
+                                                             status=models.STATUS_UNREVIEWED)
+            version.parse_metadata_json(metadata)
+            version.replace_metadata_json()
+            version.save()
 
-        transaction.commit()
-    finally:
-        transaction.set_autocommit(True)
-
-    return version, []
+            return version, []
+    except DatabaseErrorWithMessages, e:
+        return None, e.messages
 
 @login_required
 def upload_file(request):
