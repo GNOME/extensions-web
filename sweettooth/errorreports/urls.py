fc8f7e6d56fd08cb14003c1c4dc003f965c9c41e
Yuri Konotopov <ykonotopov@gnome.org>
2018-12-16 23:26:54 +0400

    comments: fixed reverse url

--- sweettooth/errorreports/urls.py
+++ sweettooth/errorreports/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import url
 from sweettooth.errorreports import views
 
 urlpatterns = [
-    url(r'^report/(?P<pk>\d+)', views.report_error),
+    url(r'^report/(?P<pk>\d+)', views.report_error, name='report_error'),
     url(r'^view/(?P<pk>\d+)', views.view_error_report),
 ]
