3349e8ef1e4bb78aec624c3f7fc3c44653db8d58
Yuri Konotopov <ykonotopov@gnome.org>
2018-12-17 22:16:58 +0400

    Revert "settings: try to simplify logging configuration"
    This reverts commit 5d4397da31ebd10d7e017d8ce9e9a090c298476c.

--- sweettooth/settings.py
+++ sweettooth/settings.py
@@ -164,19 +164,34 @@ COMMENTS_APP = 'sweettooth.ratings'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+         'require_debug_false': {
+             '()': 'django.utils.log.RequireDebugFalse'
+         }
+     },
     'handlers': {
         'console': {
             'level': 'DEBUG',
             'filters': None,
             'class': 'logging.StreamHandler',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
         }
     },
     'loggers': {
         'django': {
-            'handlers': ['console', 'mail_admins'],
+            'handlers': ['console'],
             'level': os.getenv('EGO_LOG_LEVEL', 'WARN'),
             'propagate': True,
-        }
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
     }
 }
 
