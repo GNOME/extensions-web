3047652119ee32899ab375ece0e73781022eb82e
Daniel Landau <daniel@landau.fi>
2021-04-11 23:09:51 +0300

    extensions: add missing dependency to migration

--- sweettooth/extensions/migrations/0022_extension_rating.py
+++ sweettooth/extensions/migrations/0022_extension_rating.py
@@ -41,6 +41,7 @@ def revert_data(apps, schema_editor):
 class Migration(migrations.Migration):
     dependencies = [
         ("extensions", "0021_extension_recommended"),
+        ("ratings", "0001_initial"),
     ]
 
     operations = [
