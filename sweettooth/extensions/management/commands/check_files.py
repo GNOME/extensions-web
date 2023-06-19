75d0c4907464dd5c98b9deb89d81af9cc48ddbfc
Yuri Konotopov <ykonotopov@gnome.org>
2023-06-19 23:59:13 +0400

    extensions: format check_files command

--- sweettooth/extensions/management/commands/check_files.py
+++ sweettooth/extensions/management/commands/check_files.py
@@ -2,20 +2,12 @@ from django.core.management.base import BaseCommand
 from sweettooth.extensions.models import ExtensionVersion, STATUS_ACTIVE, STATUS_REJECTED
 from zipfile import BadZipfile
 from zlib import error as ZlibError
-import codecs
+
 
 class Command(BaseCommand):
     help = "Checks consistency of extension's archives."
-    logs_directory = None
-
-    def add_arguments(self, parser):
-        parser.add_argument('logs_directory', nargs='?', help="Directory to write logs.")
-
 
     def handle(self, *args, **options):
-        if 'logs_directory' in options:
-            self.logs_directory = options['logs_directory']
-
         for version in ExtensionVersion.objects.exclude(status=STATUS_REJECTED):
             badversion = True
             try:
@@ -23,38 +15,23 @@ class Command(BaseCommand):
                     badfile = zip.testzip()
 
                     if badfile:
-                        self.error("[%s: %d] Bad entry %s in zip file" % (version.extension.name, version.version, badfile))
+                        self.stderr.write("[%s: %d] Bad entry %s in zip file" % (version.extension.name, version.version, badfile))
                     else:
                         badversion = False
 
             except IOError as e:
-                self.error("[%s: %d] Unable to find zip file: %s" % (version.extension.name, version.version, str(e)))
-            except BadZipfile as e:
-                self.error("[%s: %d] Bad zip file: %s" % (version.extension.name, version.version, version.source.name))
+                self.stderr.write("[%s: %d] Unable to find zip file: %s" % (version.extension.name, version.version, str(e)))
+            except BadZipfile:
+                self.stderr.write("[%s: %d] Bad zip file: %s" % (version.extension.name, version.version, version.source.name))
             except ZlibError as e:
-                self.error("[%s: %d] Zlib error: %s" % (version.extension.name, version.version, str(e)))
+                self.stderr.write("[%s: %d] Zlib error: %s" % (version.extension.name, version.version, str(e)))
 
             if badversion:
                 if version.extension.versions.filter(version__gt=version.version).filter(status=STATUS_ACTIVE).exists():
-                    self.message("[%s: %d] Rejecting" % (version.extension.name, version.version))
+                    self.stdout.write("[%s: %d] Rejecting" % (version.extension.name, version.version))
                 else:
-                    self.message("[%s: %d] Not rejecting: no newer active versions" % (version.extension.name, version.version))
+                    self.stdout.write("[%s: %d] Not rejecting: no newer active versions" % (version.extension.name, version.version))
 
             self.stdout.flush()
 
-        self.message('Done')
-
-
-    def message(self, string):
-        self.stdout.write(string)
-
-        if self.logs_directory:
-            with codecs.open("%s/check_files_out" % self.logs_directory, 'a', encoding='utf-8') as file:
-                file.write("%s\n" % string)
-
-    def error(self, string):
-        self.stderr.write(string)
-
-        if self.logs_directory:
-            with codecs.open("%s/check_files_err" % self.logs_directory, 'a', encoding='utf-8') as file:
-                file.write("%s\n" % string)
+        self.stdout.write('Done')
