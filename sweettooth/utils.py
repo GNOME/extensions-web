ba662b0b296f73c9f07c9b2d7a857df80adc317f
Yuri Konotopov <ykonotopov@gnome.org>
2023-06-21 18:25:36 +0400

    utils: formatted

--- sweettooth/utils.py
+++ sweettooth/utils.py
@@ -4,6 +4,7 @@ from urllib.parse import urlencode
 
 GRAVATAR_BASE = "https://secure.gravatar.com/avatar/%s?%s"
 
+
 def gravatar_url(request, email, size=70):
     email_md5 = hashlib.md5(email.lower().encode('utf-8')).hexdigest()
     options = urlencode({'d': "mm", 's': size})
