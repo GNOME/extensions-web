8ffd882ef61ed8bb315a0dc6aa5373c3af0f11f1
Yuri Konotopov <ykonotopov@gnome.org>
2023-06-21 23:38:57 +0400

    users: admin: fixed admin form

--- sweettooth/users/admin.py
+++ sweettooth/users/admin.py
@@ -7,10 +7,13 @@ from .models import User
 
 user_fieldsets = list(DefaultUserAdmin.fieldsets)
 user_fieldsets[0] = (None, {'fields': ('username', 'password', 'schedule_delete', 'force_review')})
+user_fieldsets[1] = (user_fieldsets[1][0], {'fields': ('display_name', 'email')})
 
 
 class UserAdmin(DefaultUserAdmin):
     fieldsets = user_fieldsets
+    list_display = ('username', 'email', 'display_name', 'is_staff')
+    search_fields = ('username', 'display_name', 'email')
 
 
 admin.site.register(User, UserAdmin)
