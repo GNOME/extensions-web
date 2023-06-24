249c1a8328089185829feafff83bf262b5cfdad5
Yuri Konotopov <ykonotopov@gnome.org>
2023-06-24 16:01:36 +0400

    extensions: added management command to init search indexes

---
+++ sweettooth/extensions/management/commands/init_search.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+
+from django_opensearch_dsl.registries import registry
+
+
+class Command(BaseCommand):
+    help = "Create missing search indexes"
+
+    def handle(self, *args, **options):
+        indices = registry.get_indices()
+
+        for index in indices:
+            if not index.exists():
+                index.create()
