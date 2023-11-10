0f4d45d636cb3423a7daf971f74d099c784ac4bc
Yuri Konotopov <ykonotopov@gnome.org>
2023-11-10 19:09:08 +0000

    rating: update extension `rating` and `rated` fields on new rating

--- sweettooth/ratings/models.py
+++ sweettooth/ratings/models.py
@@ -1,13 +1,26 @@
+from typing import Type
+
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django_comments.managers import CommentManager
 from django_comments.models import Comment
 from django_comments.signals import comment_will_be_posted
+
+from ..extensions.models import Extension
 
 
 class RatingComment(Comment):
     objects = CommentManager()
 
-    rating = models.IntegerField(blank=True, default=-1)
+    rating = models.IntegerField(
+        default=0,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(0),
+        ],
+    )
 
 
 def make_sure_user_was_authenticated(sender, comment, request, **kwargs):
@@ -15,3 +28,16 @@ def make_sure_user_was_authenticated(sender, comment, request, **kwargs):
 
 
 comment_will_be_posted.connect(make_sure_user_was_authenticated)
+
+
+@receiver(post_save, sender=RatingComment)
+def update_rating(
+    sender: Type[RatingComment], instance: RatingComment, created: bool, **kwargs
+):
+    if created and isinstance(instance.content_object, Extension):
+        extension = instance.content_object
+        extension.rated += 1
+        extension.rating = (
+            extension.rating * (extension.rated - 1) + instance.rating
+        ) / extension.rated
+        extension.save()
