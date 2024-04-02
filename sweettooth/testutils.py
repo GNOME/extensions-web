533ec19cf8011b468a5bd0c42b34bb62678d5947
Yuri Konotopov <ykonotopov@gnome.org>
2024-04-03 00:52:04 +0400

    extensions: test: added tests for upload API

--- sweettooth/testutils.py
+++ sweettooth/testutils.py
@@ -2,20 +2,34 @@ import logging
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from rest_framework.test import APITestCase
+
+from .auth.authentication import KnoxAuthTokenManager
+from .users.models import User
 
 
-class BasicUserTestCase(object):
+class BasicUserTestCase(TestCase):
     def setUp(self):
         super().setUp()
 
         self.username = "TestUser1"
         self.email = "non-existant@non-existant.tld"
         self.password = "a random password"
-        self.user = get_user_model().objects.create_user(
+        self.user: User = get_user_model().objects.create_user(
             self.username, self.email, self.password
         )
 
         self.client.login(username=self.username, password=self.password)
+
+
+class BasicAPIUserTestCase(APITestCase, BasicUserTestCase):
+    def setUp(self):
+        super().setUp()
+
+        manager = KnoxAuthTokenManager()
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"Token {manager.provide_token(self.user)['token']}"
+        )
 
 
 class SilentDjangoRequestTest(TestCase):
