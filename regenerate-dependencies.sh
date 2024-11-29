13b8c54324f4f0a3f1d7bb34b8231bf1e7f8bbf6
Yuri Konotopov <ykonotopov@gnome.org>
2024-11-29 15:18:31 +0000

    requirements: update requirements to support Python 3.13

--- regenerate-dependencies.sh
+++ regenerate-dependencies.sh
@@ -2,5 +2,5 @@
 
 set -exo pipefail
 
-pip-compile --allow-unsafe --generate-hashes --output-file=requirements.txt
-pip-compile --allow-unsafe --constraint=requirements.txt --extra=dev --generate-hashes --output-file=requirements.dev.txt
+pip-compile --allow-unsafe --generate-hashes --output-file=requirements.txt "${@}"
+pip-compile --allow-unsafe --constraint=requirements.txt --extra=dev --generate-hashes --output-file=requirements.dev.txt "${@}"
